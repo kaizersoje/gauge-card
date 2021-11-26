39a88165f7d6bb61a911b509601d9902021293f7
kaizersoje <54566217+kaizersoje@users.noreply.github.com>
2021-11-26 14:56:25 +1100

    Update gauge-card.js

--- gauge-card.js
+++ gauge-card.js
@@ -1,5 +1,5 @@
 console.info(
-    '%c GAUGE-CARD %c 0.2.2 ',
+    '%c GAUGE-CARD %c 0.2.3 ',
     'color: cyan; background: black; font-weight: bold;',
     'color: darkblue; background: white; font-weight: bold;',
 );
